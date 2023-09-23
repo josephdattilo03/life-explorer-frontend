a77e8017c1816e7b2a42429ed4ef79b351a04562
josephdattilo03 <jadattilobusiness@gmail.com>
2023-09-23 13:52:25 -0400

    cleaned up boilerplate formatting

--- life-explorer/src/routes/AppRouter.jsx
+++ life-explorer/src/routes/AppRouter.jsx
@@ -3,11 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../components/Home"
 
 export default function AppRouter() {
-  return (
-    <BrowserRouter>
-    <Routes>
-        <Route path="/" element={<Home></Home>}/>
-    </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home></Home>} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
